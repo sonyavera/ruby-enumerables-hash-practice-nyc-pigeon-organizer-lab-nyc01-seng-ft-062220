10a5d8ee4d0548ec9351abdd8d86a376e633e0d4
Sonya Gould <sonyavgould@gmail.com>
2020-06-15 01:08:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array)
+  final_results = data.each_with_object({}) do |(key, value), final_array|
   value.each do |inner_array, names|
     names.each do |name|
       if !final_array[name]
